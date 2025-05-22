d96b380e3d579942e5cbeffef4150b4f912e9a09
chinmay <chinmaydash11@gmail.com>
2025-05-23 02:40:15 +0530

    added callback code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare global {
+  interface Window {
+    flutter_inappwebview?: {
+      callHandler: (handlerName: string, ...args: any[]) => any;
+    };
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,6 +20,18 @@ export class AppComponent {
   title = 'my-app';
 
   onButtonClick() {
-    alert('Button was clicked!');
+    if (
+      window.flutter_inappwebview &&
+      typeof window.flutter_inappwebview.callHandler === 'function'
+    ) {
+      window.flutter_inappwebview.callHandler(
+        'FlutterWebView',
+        JSON.stringify({
+          name: 'OPEN_VISIT_APP',
+          ssoLink:
+            'https://icici.getvisitapp.net/sso?userParams=M6gfrawMWn_MPsNuEDMGLUAoutuDiyBWG3OfYPd57c0gA1wMrR21kGcrVf6qwjY0PZi0ckdqsIojT897xHHYLp3Bcjat76pyGvv0QpHQGZaFyOLOtGlUte2xP6wwctSB&clientId=icici-lomb-9641',
+        })
+      );
+    }
   }
 }
